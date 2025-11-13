6a67f00a54a6bc262859caac0661658cc2f18f83
MohawkAustinR <austin.rebelo@mohawkcollege.ca>
2025-11-13 18:40:01 -0500

    Add main menu to main scene

--- unity-project/mini-game-collection/Assets/2025/Team08/Scripts/MainMenu.cs
+++ unity-project/mini-game-collection/Assets/2025/Team08/Scripts/MainMenu.cs
@@ -6,36 +6,48 @@ using UnityEngine.SceneManagement;
 namespace MiniGameCollection.Games2025.Team08
 {
     public class MainMenu : MonoBehaviour
-    { 
+    {
+        [field: SerializeField] MiniGameManager MiniGameManager;
+
         public GameObject GameUI;
         public GameObject StartGameUI;
         public GameObject P1Ready;
         public GameObject P2Ready;
         public bool p1Ready = false;
         public bool p2Ready = false;
+        public bool hasGameStarted = false;
 
         void Update()
         {
-            // Player 1 Press Button
-            if (ArcadeInput.Players[(int)PlayerID.Player1].Action1.Pressed || ArcadeInput.Players[(int)PlayerID.Player1].Action2.Pressed)
+            // Check if the game has started
+            if (!hasGameStarted)
             {
-                P1Ready.GetComponent<TextMeshProUGUI>().text = "Player 1: Ready";
-                Debug.Log("Player 1 pressed START/FIRE");
-                p1Ready = true;
-            }
+                // Player 1 Press Button
+                if (ArcadeInput.Players[(int)PlayerID.Player1].Action1.Pressed || ArcadeInput.Players[(int)PlayerID.Player1].Action2.Pressed)
+                {
+                    P1Ready.GetComponent<TextMeshProUGUI>().text = "Player 1: Ready";
+                    p1Ready = true;
+                }
 
-            // Player 2 Press Button
-            if (ArcadeInput.Players[(int)PlayerID.Player2].Action1.Pressed || ArcadeInput.Players[(int)PlayerID.Player2].Action2.Pressed)
-            {
-                p2Ready = true;
-                P2Ready.GetComponent<TextMeshProUGUI>().text = "Player 2: Ready";
-                Debug.Log("Player 2 pressed START/FIRE");
+                // Player 2 Press Button
+                if (ArcadeInput.Players[(int)PlayerID.Player2].Action1.Pressed || ArcadeInput.Players[(int)PlayerID.Player2].Action2.Pressed)
+                {
+                    p2Ready = true;
+                    P2Ready.GetComponent<TextMeshProUGUI>().text = "Player 2: Ready";
+                }
+                if (p1Ready && p2Ready)
+                {
+                    StartGame();
+                }
             }
-            if (p1Ready && p2Ready)
-            {
-                SceneManager.LoadScene("Survive_60_Seconds");
-            }
-            
+        }
+
+        void StartGame()
+        {
+            hasGameStarted = true;
+            P1Ready.gameObject.SetActive(false);
+            P2Ready.gameObject.SetActive(false);
+            MiniGameManager.StartGame();
         }
     }
 }
